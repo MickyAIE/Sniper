c1d2279ab1537d6cd2a9d84ead98ad93941bdfa7
Dukestar1O8 <41616822+Dukestar1O8@users.noreply.github.com>
2018-09-18 15:56:45 +0800

    Main Menu/Pause Menu
    this includes the main menu with functional settings menu and the pause menu for in game

---
+++ SniperProject/Assets/LvlSelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvlSelect : MonoBehaviour {
+
+    public void Lvl1 ()
+    {
+        SceneManager.LoadScene("Lvl1");
+    }
+    public void Lvl2 ()
+    {
+        SceneManager.LoadScene("Lvl1");
+    }
+    public void Lvl3 ()
+    {
+        SceneManager.LoadScene("Lvl1");
+    }
+
+}
