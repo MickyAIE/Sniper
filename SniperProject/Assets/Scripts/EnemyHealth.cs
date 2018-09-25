613c09a41e2944566579469e676cd61e16d60c3c
GlitchyRobot <42768914+GlitchyRobot@users.noreply.github.com>
2018-09-25 18:32:09 +1000

    Ripped Bullet and Health scripts from previous project.
    You might need to edit the scripts a little bit so it can suit what the game has. Try using particles as bullets, it's better than spamming objects.

---
+++ SniperProject/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+    public float Health = 100f;
+    public GameObject NPC;
+    // Use this for initialization
+    void Start () {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        HumanoidChase HealthScript1 = NPC.GetComponent<HumanoidChase>();
+        if (HealthScript1)
+        { 
+        HealthScript1.Health = Health;
+    }
+        FlyingAI HealthScript2 = NPC.GetComponent<FlyingAI>();
+        if (HealthScript2)
+        {
+            HealthScript2.Health = Health;
+        }
+        TankAI HealthScript3 = NPC.GetComponent<TankAI>();
+        if (HealthScript3)
+        {
+            HealthScript3.Health = Health;
+        }
+        RavagerAI HealthScript4 = NPC.GetComponent<RavagerAI>();
+        if (HealthScript4)
+        {
+            HealthScript4.Health = Health;
+        }
+        GeneratorIGuess HealthScript5 = NPC.GetComponent<GeneratorIGuess>();
+        if (HealthScript5)
+        {
+            HealthScript5.Health = Health;
+        }
+        CorruptedRobotArmAI HealthScript6 = NPC.GetComponent<CorruptedRobotArmAI>();
+        if (HealthScript6)
+        {
+            HealthScript6.Health = Health;
+        }
+    }
+    public void TakeDamage(float amount)
+    {
+        Health -= amount;
+        CorruptedRobotArmAI HealthScript6 = NPC.GetComponent<CorruptedRobotArmAI>();
+        if (HealthScript6)
+        {
+            HealthScript6.PlayerHit = true;
+        }
+    }
+}
---
+++ SniperProject/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+    public float damage = 1f;
+    // Use this for initialization
+    void Start () {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+    void OnParticleCollision(GameObject other)
+    {
+           EnemyHealth targetHealth = other.GetComponent<EnemyHealth>();
+            if (targetHealth != null)
+            {
+                // Calculate the amount of damage the target should take
+                // based on it's distance from the shell.
+                // Deal this damage to the tank
+                targetHealth.TakeDamage(damage);
+            
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
